cc7d274b7ee5975bfb4433f75baa40ccf70e1ad1
Cloud User <azureuser@VM-b1.xx0y3xbzq4ruvebu2vgezyay1h.cx.internal.cloudapp.net>
2024-11-13 10:45:57 +0000

    changes done by dev1

---
+++ App.java
@@ -0,0 +1,8 @@
+//editing via dev2
+//editing via dev1
+public class App {
+    public static void main(String[] args) {
+        // Print a message to the console
+        System.out.println("Hello, welcome to the simple Java app!");
+    }
+}
